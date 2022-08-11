a99bbd1a1c8613579e7072503c7f5c4207ef1558
erhan <erhanserkansabriye@gmail.com>
2022-08-11 23:09:12 +0300

    OPERATORLER VE SORULAR

---
+++ ders/ders(operators/operators.js
@@ -0,0 +1,129 @@
+let userName = "Erhan", selamlama = "Fırat";
+console.log(`Hoşgeldiniz ${userName} ${selamlama} xyz bankası iyi günler diler `);
+
+// let x = 3 ;
+// console.log(x == "3");
+
+
+// let x = 3 ;
+// console.log(x != "3"); // Burada number ve string 3 eşit değil diye söylüyoruz false veriyor çünkü (!=) ile kullandık bu operator değer ve eşitmi değerler yönünden bakıyordu o yüzden burada eşit değil dediği için  false veriyor.
+
+// let x = 3 ;
+// console.log(x !== "3"); // Burada true veriyor  (!==) kullandık . (!) bir tane (=) yerine kullanıldı. (!==) kullandığımız için true geldi çünkü burada eşit değil bunlar dedik ve bu operatorle değer, değerler eşitmi ve type yönünden karşılaştırır.burada type farklı olduğu için eşit değiller sorusuna true dedik
+
+
+// let x = 3 ;
+// console.log(x === "3"); // False verir çünkü burada bunlar değer, type , değerleride eşit diye söylüyoruz fakat type ları aynı değil
+
+
+// 1.SORU cevap
+// let num1 = +prompt('Enter first number');
+// let num2 = +prompt('Enter second number');
+// console.log(`${num1} x ${num2} is ${num1 * num2}, \n${num1} / ${num2} is ${num1 / num2}`);
+
+// let x ,y ;
+
+// x = prompt("bir sayı yazınız: ")
+// y = prompt("bir sayı giriniz ")
+
+// console.log(x*y);
+// console.log(x/y);
+
+
+// 2.soru
+// let a ,b ,c, d ;
+// a = +prompt("a kenerını giriniz")
+// b = +prompt("c kenerını giriniz")
+// c = +prompt("c kenerını giriniz")
+// d = +prompt("d kenerını giriniz")
+
+
+// console.log("Dikdörtggen alanı:", a*b);
+
+
+// let r, pi ;
+
+// r = +prompt("Yarıçapı giriniz:" )
+
+// pi = Math.PI
+
+// console.log(pi*r**2);
+
+
+
+//3.SORU
+// let cel, fah;
+// fah = +prompt("sıcaklık ")
+// cel = +prompt("sıcaklık")
+// console.log((fah - 32)/9);
+
+// let cTemp = +prompt('Enter celcius temperature');
+// const cToFahr = (cTemp * 9) / 5 + 32;
+// console.log(`${cTemp}°C equals ${cToFahr.toFixed(2)}°F`);
+
+// let fTemp = +prompt('Enter fahrenheit temperature');
+// const fToCel = ((fTemp - 32) * 5) / 9;
+// console.log(`${fTemp}°F equals  ${fToCel.toFixed(2)}°C`);
+
+
+
+// let celcius = +prompt("celcius değeri")
+
+// fahrenheit = (9*celcius+(32*5))/5;
+// console.log(`Fahrenheit değeri = ${fahrenheit} °`)
+
+// let sayi = +prompt('donusecek sayi giriniz')
+
+// const C = (5*(sayi-32))/9;
+
+// const F = (9*sayi+(32*5))/5;
+
+// console.log(`girilen F°nin C karsılıgı => ${C}°`)
+// console.log(`girilen C°nin F karsılıgı => ${F}°`)
+
+
+
+//4.SORU
+// let time = new Date();
+
+// mount = time.getMonth();
+// day = time.getDate();
+// year = time.getFullYear();
+
+// console.log(`day of month is ${day}`);
+// console.log(`mount of year is ${mount+1}`);
+// console.log(`year is ${year}`);
+// console.log(time);
+
+
+//5.SORU
+
+// let sayı1, sayı2, sayı3;
+// sayı1 = +prompt("bır sayı giriniz: ");
+// sayı2 = +prompt("bır sayı giriniz: ");
+// sayı3 = +prompt("bır sayı giriniz: ");
+
+
+// console.log((sayı1%10 == sayı2%10) || (sayı1%10 == sayı3%10) || (sayı3%10 == sayı2%10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
